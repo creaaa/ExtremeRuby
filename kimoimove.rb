9fa6de60ed232330fe2dd6a011197dd1202931cf
crea <masataka0504@gmail.com>
2017-09-26 21:40:53 -0700

    add files

---
+++ kimoimove.rb
@@ -0,0 +1,23 @@
+
+class Sample
+	def initialize
+		@a = "a"
+	end
+
+	def a
+		@a
+	end
+
+	def sampleMethod
+
+		p a.class
+
+		if false
+			a = "kawatta"
+		end
+
+		p a().class
+	end
+end
+
+Sample.new.sampleMethod
